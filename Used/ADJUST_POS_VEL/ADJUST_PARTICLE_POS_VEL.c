a6a03b55aef157e5202b3b42f4c6d3dda7d99355
Alex Day <dayalex6@gmail.com>
2016-07-26 10:37:55 -0600

    Update ADJUST_PARTICLE_POS_VEL.c

--- Used/ADJUST_POS_VEL/ADJUST_PARTICLE_POS_VEL.c
+++ Used/ADJUST_POS_VEL/ADJUST_PARTICLE_POS_VEL.c
@@ -5,26 +5,26 @@ UDF to record information on density of cell containing particle.
 Tracks the number of particles that have a density below a threshold.
 ******************************************************************************/
 
-
+#define fluid_level 0.051625
 DEFINE_DPM_SCALAR_UPDATE(adjust_particle_pos_vel,cell,thread,initialize,p)
 {	
-	if (P_POS(p)[1] > 0.15)
+	if (P_POS(p)[1] > fluid_level)
 	{
 			printf("Particle in air. Position: %g Velocity: %g\n", P_POS(p)[1], P_VEL(p)[1]); fflush(stdout);
 			
 			// adjust particle position and velocity
-			P_POS(p)[1] = 0.15;
-			P_VEL(p)[1] = - P_VEL(p)[1];
+			P_POS(p)[1] = fluid_level;
+			P_VEL(p)[1] = - 0.5 * P_VEL(p)[1];
 			
 			printf("Particle adjusted. Position: %g Velocity: %g \n", P_POS(p)[1], P_VEL(p)[1]); fflush(stdout);
 	}
 	
-	if (P_POS(p)[1] > 0.12 && P_VEL(p)[1] > 0)
+	if (P_POS(p)[1] > (fluid_level - (fluid_level/10) && P_VEL(p)[1] > 0.01)
 	{
 		printf("Particle close to air. Position: %g Velocity: %g\n", P_POS(p)[1], P_VEL(p)[1]);	fflush(stdout);
 			
 			// adjust particle position and velocity
-			P_VEL(p)[1] = - P_VEL(p)[1];
+			P_VEL(p)[1] = - 0.5 * P_VEL(p)[1];
 			
 			printf("Particle adjusted. Position: %g Velocity: %g \n", P_POS(p)[1], P_VEL(p)[1]); fflush(stdout);
 	}
