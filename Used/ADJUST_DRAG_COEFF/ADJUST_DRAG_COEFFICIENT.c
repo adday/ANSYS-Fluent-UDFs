63a5830a62d2410e09e85f8c8c4cc21c89041666
Alexander Day <dayalex6@gmail.com>
2017-02-20 20:05:31 -0700

    Update ADJUST_DRAG_COEFFICIENT.c

--- Used/ADJUST_DRAG_COEFF/ADJUST_DRAG_COEFFICIENT.c
+++ Used/ADJUST_DRAG_COEFF/ADJUST_DRAG_COEFFICIENT.c
@@ -8,14 +8,14 @@ DEFINE_DPM_SCALAR_UPDATE(modify_drag_by_velocity, c, t, init,p)
 {
 	real alpha_q;
 	
-	// obtain volume of fraction of water
+	// obtain volume fraction of water
 	c = P_CELL(p);
 	
-	// dependent on calculation setup (0- primary phase 1-secondary phase)
+	// thread dependent on simulation parameter (0- primary phase 1-secondary phase)
 	t = THREAD_SUB_THREAD(P_CELL_THREAD(p),1);
 	alpha_q = C_VOF(c,t);
 
-	printf("\nVOF: %g\n",alpha_q);fflush(stdout);
+	printf("\n VOF: %g \n", alpha_q); fflush(stdout);
 	
 	// scale velocities ... 
 	//... of particles
@@ -31,33 +31,31 @@ DEFINE_DPM_SCALAR_UPDATE(modify_drag_by_velocity, c, t, init,p)
 
 
 /******************************************************************************
-UDF for modifying particle drag coefficient
+Modify particle drag coefficient as function of 
 ******************************************************************************/
 
 DEFINE_DPM_DRAG(modify_drag_coeff, Re, p)
 {
 	real drag_force, alpha_q;
 	
-	if (Re < 0.01)
-	{
-		drag_force = 18.0;
-	}
-	else if (Re < 20.0)
-	{
-		drag_force = 18.0 + 2.367 * pow(Re, 0.82 - 0.05 * log10(Re));
-	}
-	else
-	{
-		drag_force = 18.0 + 3.483 * pow(Re, 0.6305);
-	}
+	// set drag force based on Reynolds number
+	if (Re < 0.01) {drag_force = 18.0;}
+	else if (Re < 20.0) {drag_force = 18.0 + 2.367 * pow(Re, 0.82 - 0.05 * log10(Re));}
+	else {drag_force = 18.0 + 3.483 * pow(Re, 0.6305);}
 		
-	//obtain volume of fraction of water 
+	//obtain volume fraction of water 
 	cell_t c;
 	Thread *t;
 	c = P_CELL(p);
-	// dependent on calculation setup(0- primary phase 1-secondary phase)
+	
+	// thread dependent on simulation parameter (0- primary phase 1-secondary phase)
 	t = THREAD_SUB_THREAD(P_CELL_THREAD(p),1);
 	alpha_q = C_VOF(c,t);
-printf("\nVOF: %g \n",alpha_q);fflush(stdout);
-	return drag_force * alpha_q;
+	printf("\n VOF: %g \n", alpha_q); fflush(stdout);
+
+	drag_force = drag_force * alpha_q;
+	
+	printf("\n DF: %g \n", drag_force); fflush(stdout);
+	
+	return drag_force;
 }
