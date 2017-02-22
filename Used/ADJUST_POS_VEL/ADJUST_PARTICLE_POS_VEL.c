4183d60856771e0eaba87023dcfd5755cc6c7a17
Alexander Day <dayalex6@gmail.com>
2017-02-21 17:14:11 -0700

    Update ADJUST_PARTICLE_POS_VEL.c

--- Used/ADJUST_POS_VEL/ADJUST_PARTICLE_POS_VEL.c
+++ Used/ADJUST_POS_VEL/ADJUST_PARTICLE_POS_VEL.c
@@ -1,26 +1,30 @@
 #include "udf.h"
 
-//cuboid height = 0.10325
-#define fluid_level 0.10325 //filled cuboid
-DEFINE_DPM_SCALAR_UPDATE(adjust_particle_pos_vel,cell,thread,initialize,p)
+#define fluid_level 0.10325 // fluid level in simulation
+#define fluid_thresh 10 // upper fraction of fluid to consider
+#define vel_thresh 0.01 // velocity lower threshold for adjustment
+#define vel_adjust -0.5 // scale factor for velocity adjustment, negative for direction flip
+DEFINE_DPM_SCALAR_UPDATE( adjust_particle_pos_vel, cell, thread, initialize, p)
 {	
+	// if particle in gas phase
 	if (P_POS(p)[1] > fluid_level)
 	{
 			printf("Particle in air. Position: %g Velocity: %g\n", P_POS(p)[1], P_VEL(p)[1]); fflush(stdout);
 			
-			// adjust particle position and velocity
+			// adjust particle vertical position and velocity
 			P_POS(p)[1] = fluid_level;
-			P_VEL(p)[1] = - 0.5 * P_VEL(p)[1];
+			P_VEL(p)[1] = vel_adjust * P_VEL(p)[1];
 			
 			printf("Particle adjusted. Position: %g Velocity: %g \n", P_POS(p)[1], P_VEL(p)[1]); fflush(stdout);
 	}
 	
-	if (P_POS(p)[1] > (fluid_level - (fluid_level/10) && P_VEL(p)[1] > 0.01)
+	// if particle within 10% of gas phase and particle velocity greater than threshold
+	if (P_POS(p)[1] > (fluid_level - (fluid_level/fluid_thresh) && P_VEL(p)[1] > vel_thresh)
 	{
-		printf("Particle close to air. Position: %g Velocity: %g\n", P_POS(p)[1], P_VEL(p)[1]);	fflush(stdout);
+			printf("Particle close to air. Position: %g Velocity: %g \n", P_POS(p)[1], P_VEL(p)[1]); fflush(stdout);
 			
-			// adjust particle position and velocity
-			P_VEL(p)[1] = - 0.5 * P_VEL(p)[1];
+			// adjust particle vertical position and velocity
+			P_VEL(p)[1] = vel_adjust * P_VEL(p)[1];
 			
 			printf("Particle adjusted. Position: %g Velocity: %g \n", P_POS(p)[1], P_VEL(p)[1]); fflush(stdout);
 	}
