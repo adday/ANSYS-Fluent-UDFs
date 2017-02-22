27df181b9fa4db1394416552e15e90f24a74167c
Alexander Day <dayalex6@gmail.com>
2017-02-21 17:22:22 -0700

    Update ADJUST_PARTICLE_POS_VEL.c

--- Used/ADJUST_POS_VEL/ADJUST_PARTICLE_POS_VEL.c
+++ Used/ADJUST_POS_VEL/ADJUST_PARTICLE_POS_VEL.c
@@ -1,9 +1,14 @@
 #include "udf.h"
 
+/******************************************************************************************************************************
+Modify particle vertical position and velocity as a function of position and velocity
+******************************************************************************************************************************/
+
 #define fluid_level 0.10325 // fluid level in simulation
-#define fluid_thresh 10 // upper fraction of fluid to consider
+#define fluid_thresh 0.1 // upper fraction of fluid to consider
 #define vel_thresh 0.01 // velocity lower threshold for adjustment
 #define vel_adjust -0.5 // scale factor for velocity adjustment, negative for direction flip
+
 DEFINE_DPM_SCALAR_UPDATE( adjust_particle_pos_vel, cell, thread, initialize, p)
 {	
 	// if particle in gas phase
@@ -18,8 +23,8 @@ DEFINE_DPM_SCALAR_UPDATE( adjust_particle_pos_vel, cell, thread, initialize, p)
 			printf("Particle adjusted. Position: %g Velocity: %g \n", P_POS(p)[1], P_VEL(p)[1]); fflush(stdout);
 	}
 	
-	// if particle within 10% of gas phase and particle velocity greater than threshold
-	if (P_POS(p)[1] > (fluid_level - (fluid_level/fluid_thresh) && P_VEL(p)[1] > vel_thresh)
+	// if particle within fluid_thresh of gas phase and particle velocity greater than threshold
+	if ( (P_POS(p)[1] > (fluid_level - (fluid_level * fluid_thresh))) && (P_VEL(p)[1] > vel_thresh) )
 	{
 			printf("Particle close to air. Position: %g Velocity: %g \n", P_POS(p)[1], P_VEL(p)[1]); fflush(stdout);
 			
